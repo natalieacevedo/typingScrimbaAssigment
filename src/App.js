97e7016aef5e7108bf34ebfb8e92953fa8c63b7f
Natalie Acevedo <natalieacevedo25@gmail.com>
2022-06-06 17:04:49 +0100

    done basic css and textarea state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [textArea, setTextArea] = useState("");
+
+  console.log(textArea);
+
+  function getKeyStrokes(e) {
+    const { value } = e.target;
+    setTextArea(value);
+  }
+
   return (
-    <div className="App">
-      <h1>My react playground olee</h1>
-    </div>
+    <>
+      <h1>How fast do you type?</h1>
+      <textarea
+        name="textArea"
+        value={textArea}
+        placeholder="let me see those typing skills"
+        onChange={getKeyStrokes}
+      />
+      <h4>Time Remaining</h4>
+      <button>start Game</button>
+      <h1>Word Count</h1>
+    </>
   );
 }
 
