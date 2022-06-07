9d44c73c8fd720c574f4edf9391550ae495809e7
Natalie Acevedo <natalieacevedo25@gmail.com>
2022-06-07 15:39:27 +0100

    number of words and time functions implemented

--- src/App.js
+++ src/App.js
@@ -1,28 +1,66 @@
 import "./App.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
-  const [textArea, setTextArea] = useState("");
+  const [stateGame, setStateGame] = useState({
+    textArea: "",
+    timeRemaining: 10,
+    wordCount: 0,
+  });
 
-  console.log(textArea);
-
-  function getKeyStrokes(e) {
+  function handleTextArea(e) {
     const { value } = e.target;
-    setTextArea(value);
+    setStateGame((prev) => ({ ...prev, textArea: value }));
   }
+
+  function calculateWordCount() {
+    const wordsArr = stateGame.textArea
+      .trim()
+      .split(" ")
+      .filter((word) => word !== " ").length;
+    setStateGame((prev) => ({ ...prev, wordCount: wordsArr }));
+  }
+
+  useEffect(() => {
+    if (stateGame.timeRemaining > 0) {
+      setTimeout(() => {
+        setStateGame((prev) => ({
+          ...prev,
+          timeRemaining: prev.timeRemaining - 1,
+        }));
+      }, 1000);
+    }
+  }, [stateGame.timeRemaining]);
+
+  // useEffect(() => {
+  //   let time;
+  //   if (stateGame.timeRemaining > 0) {
+  //     time = setTimeout(() => {
+  //       stateGame((prev) => ({
+  //         ...prev,
+  //         timeRemaining: prev.timeRemaining - 1,
+  //       }));
+  //     }, 1000);
+  //     return time;
+  //   } else {
+  //     clearTimeout(time);
+  //   }
+  // }, [stateGame.timeRemaining]);
 
   return (
     <>
       <h1>How fast do you type?</h1>
       <textarea
         name="textArea"
-        value={textArea}
+        value={stateGame.textArea}
         placeholder="let me see those typing skills"
-        onChange={getKeyStrokes}
+        onChange={handleTextArea}
       />
-      <h4>Time Remaining</h4>
-      <button>start Game</button>
-      <h1>Word Count</h1>
+      <h4>Time Remaining: {stateGame.timeRemaining}</h4>
+      <button onClick={calculateWordCount}>start Game</button>
+      <h1>
+        Word Count:{stateGame.wordCount !== 0 ? stateGame.wordCount : "??"}{" "}
+      </h1>
     </>
   );
 }
